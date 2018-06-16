6979445555319c6b70a664362a395f8744673b68
Alex Khatuntsev <khatun3000@gmail.com>
2018-06-16 16:29:08 +0300

    Add unit test project

--- AccuWeatherXamarin/AccuWeatherXamarin/API.cs
+++ AccuWeatherXamarin/AccuWeatherXamarin/API.cs
@@ -3,35 +3,40 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using AccuWeatherXamarin.Models;
+using Newtonsoft.Json.Linq;
 
 namespace AccuWeatherXamarin
 {
     public static class API
     {
         static string key = "l1CyGVXxsZKVMISp6Ai3DUG8Ajh8Beex";
-        static async Task<dynamic> GetCityKey(string cityName)
+        public static async Task<string> GetCityKey(string cityName)
         {
             string query = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + key + "&q=" + "cityName";
             dynamic results = await DataService.GetDataFromService(query).ConfigureAwait(false);
-            string cityKey = (string)results[0]["Key"];
-            return cityKey;
+            //if (results[0] != null)
+            //{
+                string cityKey = (string) results[0]["Key"];
+                return cityKey;
+            //}
+            //else
+            //{
+            //    return null;
+            //}
+
+
         }
 
-        static async Task<Weather> GetWeatherForCity(string cityKey)
+        public static async Task<Weather> GetWeatherForCity(string cityKey)
         {
             string query = "http://dataservice.accuweather.com/currentconditions/v1/" + cityKey + "?apikey=" + key;
-            dynamic results = await DataService.GetDataFromService(query).ConfigureAwait(false);
-            Weather weather = new Weather();
-            try
+            JArray results = await DataService.GetDataFromService(query).ConfigureAwait(false);
+            Weather weather = new Weather
             {
-                weather.WeatherText = (string) results[0]["WeatherText"];
-                weather.IsDayTime = (string) results[0]["IsDayTime"] == "True" ? "Day" : "Night";
-                weather.Temperature = (string) results["Temperature"]["Metric"]["Value"] + " C";
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
+                WeatherText = (string)results[0]["WeatherText"],
+                IsDayTime = (string)results[0]["IsDayTime"] == "True" ? "Day" : "Night",
+                Temperature = (string)results["Temperature"]["Metric"]["Value"] + " C"
+            };
 
             return weather;
         }
--- AccuWeatherXamarin/AccuWeatherXamarin/MainPage.xaml.cs
+++ AccuWeatherXamarin/AccuWeatherXamarin/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccuWeatherXamarin.Models;
 using Xamarin.Forms;
 
 namespace AccuWeatherXamarin
@@ -12,6 +13,24 @@ namespace AccuWeatherXamarin
 		public MainPage()
 		{
 			InitializeComponent();
+		    BindingContext = new Weather();
 		}
+
+	    public async void SearchByCityButtonClicked(object sender, EventArgs e)
+	    {
+	        string cityKey = "";
+
+            try
+	        {
+	            cityKey = await API.GetCityKey("Kharkiv");
+	        }
+	        catch (Exception ex)
+	        {
+	            EntryCityName.Text = ex.Message;
+	        }
+
+	        //Weather weather = await API.GetWeatherForCity(cityKey);
+	        //BindingContext = weather;
+	    }
 	}
 }
