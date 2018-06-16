7a8c4a0ff8990f9ca8b1c2f6841f892cdbd2fb33
Alex Khatuntsev <khatun3000@gmail.com>
2018-06-16 11:58:33 +0300

    Add Receiving City Code

---
+++ AccuWeatherXamarin/AccuWeatherXamarin/API.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccuWeatherXamarin
+{
+    public static class API
+    {
+        static string key = "l1CyGVXxsZKVMISp6Ai3DUG8Ajh8Beex";
+        static async Task<dynamic> GetCityKey(string cityName)
+        {
+            string query = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + key + "&q=" + "cityName";
+            dynamic results = await DataService.GetDataFromService(query).ConfigureAwait(false);
+            string cityKey = (string)results[0]["Key"];
+            return cityKey;
+        }
+
+        //static async Task<dynamic> GetWeatherForCity(string cityKey)
+        //{
+        //    string query = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + cityKey + "?apikey=" + key;
+        //    dynamic results = await DataService.GetDataFromService(query).ConfigureAwait(false);
+        //}
+    }
+}
