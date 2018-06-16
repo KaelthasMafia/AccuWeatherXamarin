77bc5b0df973d205c869615dff55dd84fe436924
Alex Khatuntsev <khatun3000@gmail.com>
2018-06-16 12:07:36 +0300

    Add models classes for City and Weather

---
+++ AccuWeatherXamarin/AccuWeatherXamarin/Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccuWeatherXamarin.Models
+{
+    public class Weather
+    {
+        public string Text { get; set; }
+
+        public string Category { get; set; }
+
+        public string Temperature { get; set; }
+    }
+}
